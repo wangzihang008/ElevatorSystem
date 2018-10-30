c2dd8651ceabfb9778beaab16b03fb71f9ad4799
trainee1 <trainee1@LAPSHA00001.fdmgroup.local>
2018-10-30 17:29:05 +0800

    modified App.java, created Person.java and State.java

---
+++ src/main/java/com/fdmgroup/ElevatorSystem/Person.java
@@ -0,0 +1,24 @@
+package com.fdmgroup.ElevatorSystem;
+
+public class Person {
+	
+	private int startFloor;
+	private int destinationFloor;
+	
+	public Person(int startFloor, int destinationFloor) {
+		
+		this.startFloor = startFloor;
+		this.destinationFloor = destinationFloor;
+	}
+	
+	public int getStartFloor() {
+		
+		return startFloor;
+	}
+	
+	public int getDestinationFloor() {
+		
+		return destinationFloor;
+	}
+
+}
