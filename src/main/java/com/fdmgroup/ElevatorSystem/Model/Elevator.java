d5ca6c517151bd1ca67fb79a329df22540bd486f
trainee1 <trainee1@LAPSHA00010.fdmgroup.local>
2018-10-31 14:57:07 +0800

    Modified Elevator.java & WaitingPeople.java & log4j.properties & ElevatorTest.java

--- src/main/java/com/fdmgroup/ElevatorSystem/Model/Elevator.java
+++ src/main/java/com/fdmgroup/ElevatorSystem/Model/Elevator.java
@@ -57,8 +57,8 @@ public class Elevator implements Runnable{
 		people.add(person);
 	}
 	
-	public void deliverPassager() {
-		people.peek();
+	public Person deliverPassager() {
+		return people.poll();
 	}
 	
 	public boolean isFull() {
