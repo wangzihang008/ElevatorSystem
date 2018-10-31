5adce5c90688ec61e15221f7e68035f605ef5d69
trainee1 <trainee1@LAPSHA00001.fdmgroup.local>
2018-10-31 09:21:40 +0800

    updated by qz

--- src/main/java/com/fdmgroup/ElevatorSystem/State.java
+++ src/main/java/com/fdmgroup/ElevatorSystem/State.java
@@ -2,6 +2,18 @@ package com.fdmgroup.ElevatorSystem;
 
 public enum State {
 	
+	SEEDPUP(1), SLOWDOWN(1), MOVE(1), SERVICE(2), STOP(0);
 	
+	private int time;
+	
+	private State(int time) {
+		
+		this.time = time;
+	}
+	
+	public int getTime() {
+		
+		return time;
+	}
 
 }
