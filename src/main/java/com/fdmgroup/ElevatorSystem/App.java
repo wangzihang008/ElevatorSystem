6cca06e5d5b3c7f2bf23984929437f87f849a5f8
trainee1 <trainee1@LAPSHA00010.fdmgroup.local>
2018-10-31 13:27:41 +0800

    Modified App.java & ElevatorController.java & ElevatorView.java

--- src/main/java/com/fdmgroup/ElevatorSystem/App.java
+++ src/main/java/com/fdmgroup/ElevatorSystem/App.java
@@ -1,5 +1,9 @@
 package com.fdmgroup.ElevatorSystem;
 
+import java.util.Scanner;
+
+import com.fdmgroup.ElevatorSystem.Controller.ElevatorController;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello World!");
+        String totalFloor;
+        String numElevators;
+        String elevatorLimit;
+    	
+    	Scanner scanner = new Scanner(System.in);
+    	System.out.println("Welcome to Smart Building, please initailise building first");
+        System.out.print("Number of floors are : ");
+        totalFloor = scanner.nextLine();
+        System.out.print("Number of elevators are : ");
+        numElevators = scanner.nextLine();
+        System.out.print("Max number of elevator are : ");
+        elevatorLimit = scanner.nextLine();
+        
+        ElevatorController elevatorController = 
+        		new ElevatorController(Integer.parseInt(totalFloor), Integer.parseInt(numElevators), Integer.parseInt(elevatorLimit));
+        
     }
 }
--- src/main/java/com/fdmgroup/ElevatorSystem/Controller/ElevatorController.java
+++ src/main/java/com/fdmgroup/ElevatorSystem/Controller/ElevatorController.java
@@ -11,18 +11,25 @@ import com.fdmgroup.ElevatorSystem.View.ElevatorView;
 public class ElevatorController {
 	private ElevatorView elevatorWiew;
 	private WaitingPeople waitingPeople;
+	private List<Thread> elevatorThreads;
 	private List<Elevator> elevators;
 	private int totalFloor;
 	
 	public ElevatorController(int totalFloor, int numElevator, int elevatorLimit) {
 		this.totalFloor = totalFloor;
 		elevators = new ArrayList<Elevator>();
+		elevatorThreads = new ArrayList<Thread>();
 		waitingPeople = new WaitingPeople();
 		elevatorWiew = new ElevatorView(this);
 		
 		for(int i = 0; i < numElevator; i++) {
 			Elevator e = new Elevator(elevatorLimit);
+			e.setName("Elevator " + (i + 1));
+			Thread t = new Thread(e);
 			elevators.add(e);
+			elevatorThreads.add(t);
+			
+			t.start();
 		}
 	}
 	
