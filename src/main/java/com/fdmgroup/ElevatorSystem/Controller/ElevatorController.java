707b93da1f974c039c982d89d62f180b0c33052d
trainee1 <trainee1@LAPSHA00001.fdmgroup.local>
2018-10-31 16:34:21 +0800

    created findBestArrangement and putPeopleINELevators in Controller

--- src/main/java/com/fdmgroup/ElevatorSystem/Controller/ElevatorController.java
+++ src/main/java/com/fdmgroup/ElevatorSystem/Controller/ElevatorController.java
@@ -1,7 +1,12 @@
 package com.fdmgroup.ElevatorSystem.Controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 import com.fdmgroup.ElevatorSystem.Model.Elevator;
 import com.fdmgroup.ElevatorSystem.Model.Person;
@@ -36,4 +41,47 @@ public class ElevatorController {
 	public void addNewPassengerIntoWaitingQueue(Person person) {
 		
 	}
+	
+	public Map<Map<Elevator, Queue<Person>>, Integer> findBestArrangement(List<Map<Elevator, Queue<Person>>> allPossibility) {
+		
+		Map<Map<Elevator, Queue<Person>>, Integer> result = new HashMap<Map<Elevator, Queue<Person>>, Integer>();
+		Map<Elevator, Queue<Person>> bestMap = allPossibility.get(0);
+		int minTimeAmongAllPossibility = 2147483647;
+		for (Map<Elevator, Queue<Person>> m : allPossibility) {
+			
+			Set<Elevator> keySet = m.keySet();
+			Iterator<Elevator> keyIter = keySet.iterator();
+			int maxTimeAmongELevators = 0;
+			while (keyIter.hasNext()) {
+				
+				Elevator elevatorToCalculate = keyIter.next();
+				int runTime = elevatorToCalculate.calculateTime(m.get(elevatorToCalculate));
+				if (runTime > maxTimeAmongELevators) {
+					
+					maxTimeAmongELevators = runTime;
+				}
+			}
+			int time = maxTimeAmongELevators;
+			if (time < minTimeAmongAllPossibility) {
+				
+				minTimeAmongAllPossibility = time;
+				bestMap = m;
+			}
+		}
+		result.put(bestMap, minTimeAmongAllPossibility);
+		
+		return result;
+	}
+	
+	public void putPersonIntoElevators(Map<Elevator, Queue<Person>> bestMap) {
+		
+		for (Elevator e : elevators) {
+			
+			e.setPeople(bestMap.get(e));
+		}
+	}
+	
+	
+	
+	
 }
--- src/main/java/com/fdmgroup/ElevatorSystem/Model/Elevator.java
+++ src/main/java/com/fdmgroup/ElevatorSystem/Model/Elevator.java
@@ -1,6 +1,5 @@
 package com.fdmgroup.ElevatorSystem.Model;
 
-import java.util.LinkedList;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 
